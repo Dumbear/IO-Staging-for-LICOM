7298c86c94a95738b50ed7b6c368f852a6fb5cd8
Yaxiong Liang <dumbear@163.com>
2014-04-09 22:46:18 +0800

    Update submit script

--- src/test_01/submit.sh
+++ src/test_01/submit.sh
@@ -4,16 +4,17 @@ source ../env_config.sh
 
 PROC_WRITER=1
 PROC_SERVER=1
-WRITE_SIZE=1
-let "TOTAL_SIZE=WRITE_SIZE*1024*1024"
+DATA_SIZE_X=1024
+DATA_SIZE_Y=1024
+DATA_SIZE_Z=1024
 
 rm -f conf dataspaces.conf
 
 echo "# Config file for DataSpaces
 ndim = 3
-dimx = $TOTAL_SIZE
-dimy = 1
-dimz = 1
+dimx = $DATA_SIZE_X
+dimy = $DATA_SIZE_Y
+dimz = $DATA_SIZE_Z
 max_versions = 2
 " > dataspaces.conf
 
@@ -34,7 +35,7 @@ done < conf
 echo "DataSpaces IDs: P2TNID = $P2TNID, P2TPID = $P2TPID"
 
 echo "Start writer on $PROC_WRITER PEs"
-bsub -q normal -a intelmpi -n $PROC_WRITER -o log.writer mpirun.lsf ./writer $WRITE_SIZE
+bsub -q normal -a intelmpi -n $PROC_WRITER -o log.writer mpirun.lsf ./writer $DATA_SIZE_X $DATA_SIZE_Y $DATA_SIZE_Z 1 1 1
 
 echo "Wait until all applications exit."
 wait
