bc3c91f6821552ec092e48feba780b3bd12c9250
Yaxiong Liang <dumbear@163.com>
2014-05-05 20:21:05 +0800

    Make a new branch for common files on both platforms

--- src/env_config.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# ADIOS
-PATH=$PATH:/home/zouevan/soft/adios-1.6.0/bin
-
-# DataSpaces
-PATH=$PATH:/home/zouevan/soft/dataspaces-1.3.0/bin
-DATASPACES=/home/zouevan/soft/dataspaces-1.3.0/bin/dataspaces_server
