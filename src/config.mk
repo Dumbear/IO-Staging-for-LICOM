2666b6eacc6b0f82b2cb8d4950c9ec46f07046ce
Yaxiong Liang <dumbear@163.com>
2014-05-17 11:46:33 +0800

    Modify environments

--- src/config.mk
+++ src/config.mk
@@ -1,7 +1,7 @@
 ADIOS_DIR=/home/xuewei/WORK/liangyaxiong/softwares/adios-1.6.0
 ADIOS_CONFIG=${ADIOS_DIR}/bin/adios_config
 
-CC = mpiicpc
-cc = mpiicc
+CXX = mpiicpc
+CC = mpiicc
 LDFLAGS = `${ADIOS_CONFIG} -l` -lskel
 CFLAGS  = -g -O2 `${ADIOS_CONFIG} -c`
