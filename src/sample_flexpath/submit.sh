55847e50b60b699eab681cabe947e83a3a28c2c3
Yaxiong Liang <dumbear@163.com>
2014-05-30 14:57:22 +0800

    Add submit script

---
+++ src/sample_flexpath/submit.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+source ../env_config.sh
+
+PROC_WRITER=8
+PROC_READER=1
+
+rm *_info.txt *_ready.txt
+
+echo "Start writer on $PROC_WRITER PEs"
+mpiexec -host cn001 -n $PROC_WRITER ./writer >& log.writer &
+
+echo "Start reader on $PROC_READER PEs"
+mpiexec -host cn001 -n $PROC_READER ./reader >& log.reader &
+
+echo "Wait until all applications exit."
+wait
