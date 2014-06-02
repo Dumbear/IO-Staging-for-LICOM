151a4ee31f39c8217181831f8b731b26c9297b62
Yaxiong Liang <dumbear@163.com>
2014-06-02 14:54:56 +0800

    Add a new bug sample for wrong dimension ordering

---
+++ src/sample_flexpath/bug_dimension_ordering/reader.cpp
@@ -0,0 +1,52 @@
+/******************************************************************************\
+*                         Author:  Dumbear                                     *
+*                         Email:   dumbear[#at]163.com                         *
+*                         Website: http://dumbear.com                          *
+\******************************************************************************/
+#include "mpi.h"
+#include "adios.h"
+#include "adios_read.h"
+#include <cstdio>
+#include <cstring>
+
+using namespace std;
+
+int proc_rank, proc_size;
+ADIOS_FILE *fp_in;
+
+uint64_t dim_start[10], dim_count[10];
+ADIOS_SELECTION *sel;
+int data[10];
+
+int main(int argc, char **argv) {
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &proc_rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &proc_size);
+
+    dim_start[0] = 0; dim_start[1] = 0; dim_start[2] = 0;
+    dim_count[0] = 2; dim_count[1] = 2; dim_count[2] = 2;
+    sel = adios_selection_boundingbox(3, dim_start, dim_count);
+
+    adios_read_init_method(ADIOS_READ_METHOD_FLEXPATH, MPI_COMM_WORLD, "");
+    fp_in = adios_read_open("sample.bp", ADIOS_READ_METHOD_FLEXPATH, MPI_COMM_WORLD, ADIOS_LOCKMODE_NONE, 0.0);
+
+    ADIOS_VARINFO *nx_info = adios_inq_var(fp_in, "nx");
+    ADIOS_VARINFO *ny_info = adios_inq_var(fp_in, "ny");
+    ADIOS_VARINFO *nz_info = adios_inq_var(fp_in, "nz");
+    printf("nx = %d\n", *(int *)nx_info->value);
+    printf("ny = %d\n", *(int *)ny_info->value);
+    printf("nz = %d\n", *(int *)nz_info->value);
+
+    memset(data, -1, sizeof(data));
+    adios_schedule_read(fp_in, sel, "data", 0, 1, data);
+    adios_perform_reads(fp_in, 1);
+    for (int i = 0; i < 8; ++i) {
+        printf(i == 7 ? "%d\n" : "%d, ", data[i]);
+    }
+
+    adios_read_close(fp_in);
+    adios_read_finalize_method(ADIOS_READ_METHOD_FLEXPATH);
+
+    MPI_Finalize();
+    return 0;
+}
